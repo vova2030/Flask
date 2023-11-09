336f408221a95bb9de655d5ac9b3b0c5e371947a
Volodia Terletskiy <vol.terlia@gmail.com>
2023-11-09 13:02:30 +0200

    update 1.0.2 (Jinja)

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    return render_template("base.html", title="Jinja and Flask")
+
+  
+max_score = 100   
+test_name = "Python Challenge"  
+students = [  
+    {"name": "Любомир", "score": 100},
+    {"name": "Юра", "score": 87}, 
+    {"name": "Володя", "score": 92},   
+    {"name": "Віталій", "score": 40},
+    {"name": "Денис", "score": 75},   
+] 
 
 
-if __name__ == '__main__':
+@app.route("/results")
+def results():
+    context = {
+        "title": "Results",
+        "students": students,
+        "test_name": test_name,
+        "max_score": max_score,
+    }
+    return render_template("results.html", **context)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
