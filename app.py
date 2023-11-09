d2176a9d6fffbc3d2c5e42f08e4f0ded3b401f0f
Volodia Terletskiy <vol.terlia@gmail.com>
2023-11-09 11:50:16 +0200

    update1.0.1

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 
 @app.route('/')
+def index():
+    return render_template('index.html')
 
-def welcome():
-    return "Hi a`m volodia "
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
